4356cc09074659e09e49ba3d9c5304035f97cf12
Ismail-Tuychiyev <ismoiltuychiyev022@gmail.com>
2025-10-01 12:22:16 +0500

    amazing

---
+++ products.js
@@ -0,0 +1,70 @@
+const products = [
+  {
+    slug: "termiz-carton",
+     code: "A-4",     
+    title: "Termiz carton",
+    price: 16500,
+    badge: "NEW",
+    images: {
+      main: "images/IMG_5459.JPG",
+      hover: "images/IMG_5460.JPG"
+    },
+    colors: [
+      {name:"Black", hex:"#0a0a0a", image:"images/IMG_5459.JPG"},
+      {name:"Army Green", hex:"#8a8a6a", image:"images/IMG_5467.JPG"}
+    ],
+    sizes: ["S","M","L","XL","XXL"]
+  },
+
+  {
+    slug: "termiz-ayiq",
+    code: "A-3",     
+    title: "Termiz ayiq",
+    price: 16500,
+    badge: "NEW",
+    images: {
+      main: "images/IMG_5462.JPG",
+      hover: "images/IMG_5463.JPG"
+    },
+    colors: [
+      {name:"Black", hex:"#0a0a0a", image:"images/IMG_5462.JPG"},
+      {name:"Army Green", hex:"#8a8a6a", image:"images/IMG_5463.JPG"}
+    ]
+  },
+
+  {
+    slug: "termiz-robot",
+    code: "A-2",     
+    title: "Termiz robot",
+    price: 16500,
+    badge: "",
+    images: {
+      main: "images/IMG_5466.JPG",
+      hover: "images/IMG_5467.JPG"
+    },
+    colors: [
+      {name:"Black", hex:"#0a0a0a", image:"images/IMG_5466.JPG"},
+      {name:"Army Green", hex:"#8a8a6a", image:"images/IMG_5467.JPG"}
+    ]
+  },
+
+  {
+    slug: "termiz-cool",
+    code: "A-1",     
+    title: "Termiz cool",
+    price: 16500,
+    badge: "",
+    images: {
+      main: "images/IMG_5479.JPG",
+      hover: "images/IMG_5480.JPG"
+    },
+    colors: [
+      {name:"Black", hex:"#b8b8b8ff", image:"images/IMG_5479.JPG"},
+      {name:"Army Green", hex:"#ff2626ff", image:"images/IMG_5480.JPG",},
+      {name:"Army Green", hex:"#00ff99ff", image:"images/IMG_5480.JPG",},
+    ]
+  }
+  
+];
+
+window.products = products;
